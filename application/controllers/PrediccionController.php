77ded15b93a9eb4385c2fe8d0357e5ff1c65c88c
Hugo Montes <hmontes@cofar.com.bo>
2018-06-04 10:04:16 -0400

    Se ha finalizado con el modulo de prediccion

--- application/controllers/PrediccionController.php
+++ application/controllers/PrediccionController.php
@@ -5,6 +5,16 @@ class PrediccionController extends CI_Controller {
 
     private $title;
 
+    private $dias;
+    private $prom_pp=array();
+    private $prom_med=array();
+    private $prom_max=array();
+    private $prom_min=array();
+    private $abs_pp=array();
+    private $abs_med=array();
+    private $abs_max=array();
+    private $abs_min=array();
+
     function __construct(){
         parent::__construct();
         $this->login->verifyUser();
@@ -18,102 +28,92 @@ class PrediccionController extends CI_Controller {
         if($countanios<MIN_ANIOS){
             $this->session->set_flashdata('warning', 'Para realizar una predicción necesita como minimo los datos de tres años agricolas.');
         }
-        $data['fecha']=date("d/m/Y");        
+        $data['meses']=array('JULIO'=>'JULIO','AGOSTO'=>'AGOSTO','SEPTIEMBRE'=>'SEPTIEMBRE','OCTUBRE'=>'OCTUBRE','NOVIEMBRE'=>'NOVIEMBRE','DICIEMBRE'=>'DICIEMBRE','ENERO'=>'ENERO','FEBRERO'=>'FEBRERO','MARZO'=>'MARZO','MARZO'=>'MARZO','ABRIL'=>'ABRIL','MAYO'=>'MAYO','JUNIO'=>'JUNIO');        
+        $data['messelect']='JULIO';
+        $data['dias_mes']=$this->generarDias();
         $data['title']=$this->title;
+        $data['diaselect']=1;
+        //print_r($this->generarDias());
         $this->load->view('prediccion/formulario_view', $data);       
     }
 
-    private $dias;
-    // private $deanios=array(2,3,4,5,6,7);
-    private $prom_pp=array();
-    private $prom_med=array();
-    private $prom_max=array();
-    private $prom_min=array();
-
-    private $abs_pp=array();
-    private $abs_med=array();
-    private $abs_max=array();
-    private $abs_min=array();
+    private function generarDias(){
+        $dias_mes=array();
+        for($i=1; $i<=31; $i++){
+            $dias_mes[$i]=$i;
+        }
+        return $dias_mes;
+    }
 
     public function calcularAction(){
         $countanios=$this->aniosmodel->countAll();
-        $fecha=date("d/m/Y");
         if($countanios<MIN_ANIOS){
             $this->session->set_flashdata('error', 'Para realizar una predicción necesita como minimo los datos de tres años agricolas.');
         }else{
             // $fecha=$this->input->post('fecha');
-            $this->dias=$this->anioagricolamodel->findByMesDia('JULIO', 1);
+            $mes_form=$this->input->post('mes');
+            $dia_form=$this->input->post('dia');
+
+            $this->dias=$this->anioagricolamodel->findByMesDia($mes_form, $dia_form);
             // print_r($this->dias);
             $this->calcularPromedios(1, 0, 0, 0, 0);
             //print_r($this->deanios); echo '<br/>';
             
-            print_r($this->abs_pp); 
             $minimo_pp=min($this->abs_pp);
-            echo ' ---- '.$minimo_pp.'<br/>';
-
-            print_r($this->abs_med); 
             $minimo_med=min($this->abs_med);
-            echo ' ---- '.$minimo_med.'<br/>';
-            
-            print_r($this->abs_max); 
             $minimo_max=min($this->abs_max);
-            echo ' ---- '.$minimo_max.'<br/>';
-            
-            print_r($this->abs_min); 
             $minimo_min=min($this->abs_min);
-            echo ' ---- '.$minimo_min.'<br/>';
 
             $ca_pp=$this->cuantosAnios($this->abs_pp, 0, $minimo_pp, count($this->abs_pp));
             $ca_med=$this->cuantosAnios($this->abs_med, 0, $minimo_med, count($this->abs_med));
             $ca_max=$this->cuantosAnios($this->abs_max, 0, $minimo_max, count($this->abs_max));
             $ca_min=$this->cuantosAnios($this->abs_min, 0, $minimo_min, count($this->abs_min));
 
-            echo '<br/>';
-            echo 'TEMP. PP - CUANTOS AÑOS: '.$ca_pp;
-            echo '<br/>';
-            echo 'TEMP. MEDIA - CUANTOS AÑOS: '.$ca_med;
-            echo '<br/>';
-            echo 'TEMP. MAX - CUANTOS AÑOS: '.$ca_max;
-            echo '<br/>';
-            echo 'TEMP. MIN - CUANTOS AÑOS: '.$ca_min;
+            
+            //echo '<br/>';
+            //echo 'TEMP. PP - CUANTOS AÑOS: '.$ca_pp;
+            //echo '<br/>';
+            //echo 'TEMP. MEDIA - CUANTOS AÑOS: '.$ca_med;
+            //echo '<br/>';
+            //echo 'TEMP. MAX - CUANTOS AÑOS: '.$ca_max;
+            //echo '<br/>';
+            //echo 'TEMP. MIN - CUANTOS AÑOS: '.$ca_min;
             
 
             $esperado_pp=$this->esperado($this->prom_pp, 0, $ca_pp, count($this->prom_pp));
             $esperado_med=$this->esperado($this->prom_med, 0, $ca_med, count($this->prom_med));
             $esperado_max=$this->esperado($this->prom_max, 0, $ca_max, count($this->prom_max));
             $esperado_min=$this->esperado($this->prom_min, 0, $ca_min, count($this->prom_min));
+  
+            $data['esperados']=array(
+                'pp'=>$esperado_pp,
+                'med'=>$esperado_med,
+                'max'=>$esperado_max,
+                'min'=>$esperado_min,
+            );           
 
-            echo '<br/><br/>';
-            echo 'TEMP. PP - ESPERADO: '.$esperado_pp;
-            echo '<br/>';
-            echo 'TEMP. MEDIA - ESPERADO: '.$esperado_med;
-            echo '<br/>';
-            echo 'TEMP. MAX - ESPERADO: '.$esperado_max;
-            echo '<br/>';
-            echo 'TEMP. MIN - ESPERADO: '.$esperado_min;        
-
-            echo '<br/><br/>';
-            echo 'TEMP. PP - VALOR MENOR: '.($esperado_pp-$minimo_pp);
-            echo '<br/>';
-            echo 'TEMP. MEDIA - VALOR MENOR: '.($esperado_med-$minimo_med);
-            echo '<br/>';
-            echo 'TEMP. MAX - VALOR MENOR: '.($esperado_max-$minimo_max);
-            echo '<br/>';
-            echo 'TEMP. MIN - VALOR MENOR: '.($esperado_min-$minimo_min);       
-
-            echo '<br/><br/>';
-            echo 'TEMP. PP - VALOR MAYOR: '.($esperado_pp+$minimo_pp);
-            echo '<br/>';
-            echo 'TEMP. MEDIA - VALOR MAYOR: '.($esperado_med+$minimo_med);
-            echo '<br/>';
-            echo 'TEMP. MAX - VALOR MAYOR: '.($esperado_max+$minimo_max);
-            echo '<br/>';
-            echo 'TEMP. MIN - VALOR MAYOR: '.($esperado_min+$minimo_min);     
+            $data['menores']=array(
+                'pp'=>($esperado_pp-$minimo_pp),
+                'med'=>($esperado_med-$minimo_med),
+                'max'=>($esperado_max-$minimo_max),
+                'min'=>($esperado_min-$minimo_min),
+            );
+            
+            $data['mayores']=array(
+                'pp'=>($esperado_pp+$minimo_pp),
+                'med'=>($esperado_med+$minimo_med),
+                'max'=>($esperado_max+$minimo_max),
+                'min'=>($esperado_min+$minimo_min),
+            );
         }
-        //$data['fecha']=$fecha;        
-        //$data['title']=$this->title;
-        //$this->load->view('prediccion/formulario_view', $data);    
+        $data['meses']=array('JULIO'=>'JULIO','AGOSTO'=>'AGOSTO','SEPTIEMBRE'=>'SEPTIEMBRE','OCTUBRE'=>'OCTUBRE','NOVIEMBRE'=>'NOVIEMBRE','DICIEMBRE'=>'DICIEMBRE','ENERO'=>'ENERO','FEBRERO'=>'FEBRERO','MARZO'=>'MARZO','MARZO'=>'MARZO','ABRIL'=>'ABRIL','MAYO'=>'MAYO','JUNIO'=>'JUNIO');        
+        $data['messelect']=$mes_form;
+        $data['dias_mes']=$this->generarDias();
+        $data['diaselect']=$dia_form;    
+        $data['title']=$this->title;
+        $this->load->view('prediccion/formulario_view', $data);    
     }
+
 /*
     private function calcularPromedios($i, $sum_pp, $sum_med, $sum_max, $sum_min){
         if($i<count($this->dias)){
--- application/views/prediccion/formulario_view.php
+++ application/views/prediccion/formulario_view.php
@@ -1,89 +1,123 @@
 <?php $this->load->view('template/header');?>
-
-<div class="row">
-   <div class="col-md-6">
-      <div class="box box-primary">
-         <div class="box-body">
-            <?php echo form_open('prediccion/calcular', array('role'=>'form')); ?>
-            <div class="form-group">
-               <?php echo form_label('Seleccionar fecha:', 'fecha'); ?>
-               <div class="input-group date">
-                  <div class="input-group-addon">
-                     <i class="fa fa-calendar"></i>
-                  </div>
-                  <?php echo form_input(array('id'=>'fecha',
-                                             'name'=>'fecha',
-                                             'value'=>set_value('fecha',$fecha),
-                                             'placeholder'=>'dd/mm/aa',
-                                             'class'=>'form-control pull-right')); ?>
-               </div>
+<div class="box box-primary">
+    <div class="box-body">
+        <?php echo form_open('prediccion/calcular', array('role'=>'form')); ?>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <?php echo form_label('Seleccionar Mes:', 'mes'); ?>
+                    <div class="input-group date">
+                        <div class="input-group-addon">
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                        <?php echo form_dropdown('mes', $meses, $messelect, array('id'=>'mes','class'=>'form-control')); ?>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-               <div style="text-align: right;">
-                  <?php echo form_submit(array('value'=>'Realizar Predicción',
-                                                        'class'=>'btn btn-primary')); ?>
-               </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <?php echo form_label('Seleccionar Dia:', 'dia'); ?>
+                    <div class="input-group date">
+                        <div class="input-group-addon">
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                        <?php echo form_dropdown('dia', $dias_mes, $diaselect, array('id'=>'dia','class'=>'form-control')); ?>
+                    </div>
+                </div>
             </div>
-            <?php echo form_close(); ?>
-         </div>
-         <!-- /.box-body -->
-      </div>
-      <!-- /.box -->
-   </div>
+            <div class="col-md-12">
+                <div class="form-group">
+                    <div style="text-align: right;">
+                        <?php echo form_submit(array('value'=>'Realizar Predicción','class'=>'btn btn-primary')); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php echo form_close(); ?>
+    </div>
+    <!-- /.box-body -->
 </div>
 
+
+<?php if(isset($menores) and isset($esperados) and isset($mayores)){ ?>
 <div class="row">
-   <div class="col-md-12">
-      <div class="box box-success">
-         <div class="box-header">
-              <h3 class="box-title">Resultados de Predicción</h3>
-         </div>
-         <div class="box-header with-border">
-            <table class="table table-bordered table-striped">
-               <thead>
-                  <tr>
-                     <th></th>
-                     <th style="text-align:center;">VALOR MENOR</th>
-                     <th style="text-align:center;">VALOR ESPERADO</th>
-                     <th style="text-align:center;">VALOR MAYOR</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  <tr>
-                     <th>PRECIPITACIÓN</th>
-                     <td></td>
-                     <td></td>
-                     <td></td>
-                  </tr>
-                  <tr>
-                     <th>TEMPERATURA MAXIMA</th>
-                     <td></td>
-                     <td></td>
-                     <td></td>
-                  </tr>
-                  <tr>
-                     <th>TEMPERATURA MEDIA</th>
-                     <td></td>
-                     <td></td>
-                     <td></td>
-                  </tr>
-                  <tr>
-                     <th>TEMPERATURA MINIMA</th>
-                     <td></td>
-                     <td></td>
-                     <td></td>
-                  </tr>
-               </tbody>
-            </table>
-         </div>
-      </div>
-   </div>
+    <div class="col-md-12">
+        <div class="box box-success">
+            <div class="box-header">
+                <h3 class="box-title">Resultados de Predicción</h3>
+            </div>
+            <div class="box-header with-border">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th style="text-align:center;">VALOR MENOR</th>
+                            <th style="text-align:center;">VALOR ESPERADO</th>
+                            <th style="text-align:center;">VALOR MAYOR</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>PRECIPITACIÓN</th>
+                            <td style="text-align:right;">
+                                <?php echo number_format($menores['pp'], 1); ?>
+                            </td>
+                            <td style="text-align:right;">
+                                <?php echo number_format($esperados['pp'], 1); ?>
+                            </td>
+                            <td style="text-align:right;">
+                                <?php echo number_format($mayores['pp'], 1); ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>TEMPERATURA MAXIMA</th>
+                            <td style="text-align:right;">
+                                <?php echo number_format($menores['max'], 1); ?>
+                            </td>
+                            <td style="text-align:right;">
+                                <?php echo number_format($esperados['max'], 1); ?>
+                            </td>
+                            <td style="text-align:right;">
+                                <?php echo number_format($mayores['max'], 1); ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>TEMPERATURA MEDIA</th>
+                            <td style="text-align:right;">
+                                <?php echo number_format($menores['med'], 1); ?>
+                            </td>
+                            <td style="text-align:right;">
+                                <?php echo number_format($esperados['med'], 1); ?>
+                            </td>
+                            <td style="text-align:right;">
+                                <?php echo number_format($mayores['med'], 1); ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>TEMPERATURA MINIMA</th>
+                            <td style="text-align:right;">
+                                <?php echo number_format($menores['min'], 1); ?>
+                            </td>
+                            <td style="text-align:right;">
+                                <?php echo number_format($esperados['min'], 1); ?>
+                            </td>
+                            <td style="text-align:right;">
+                                <?php echo number_format($mayores['min'], 1); ?>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
+<?php } ?>
+
 <?php $this->load->view('template/footer');?>
 <script>
-   //Date picker
-   $('#fecha').datepicker({
-      autoclose: true,
-      language: 'es'
-   });
+    //Date picker
+    $('#fecha').datepicker({
+        autoclose: true,
+        language: 'es'
+    });
 </script>
