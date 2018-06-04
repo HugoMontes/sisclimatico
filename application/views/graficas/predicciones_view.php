d516464c2255d296796cf9008936d108bf0cbea5
Hugo Montes <hmontes@cofar.com.bo>
2018-06-04 15:26:53 -0400

    Se iniciaron plugins para graficas

--- application/views/graficas/predicciones_view.php
+++ application/views/graficas/predicciones_view.php
@@ -1,4 +1,6 @@
 <?php $this->load->view('template/header');?>
+<!-- Morris charts -->
+<link rel="stylesheet" href="<?php echo base_url();?>resources/bower_components/morris.js/morris.css">
 
 <div class="row">
     <div class="col-md-6">
@@ -26,18 +28,49 @@
     </div>
 </div>
 
+<div class="box box-success">
+    <div class="box-header with-border">
+        <h3 class="box-title">GRAFICA DE TEMPERATURA MEDIA</h3>
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="box box-success">
-            <div class="box-header">
-                <h3 class="box-title">Grafica de Predicción</h3>
-            </div>
-            <div class="box-header with-border">
-
-            </div>
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                <i class="fa fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove">
+                <i class="fa fa-times"></i>
+            </button>
         </div>
     </div>
+    <div class="box-body chart-responsive">
+        <div class="chart" id="line-chart" style="height: 300px;"></div>
+    </div>
+    <!-- /.box-body -->
 </div>
-
 <?php $this->load->view('template/footer');?>
+<!-- Morris.js charts -->
+<script src="<?php echo base_url();?>resources/bower_components/raphael/raphael.min.js"></script>
+<script src="<?php echo base_url();?>resources/bower_components/morris.js/morris.min.js"></script>
+<script>
+    // LINE CHART
+    var line = new Morris.Line({
+      element: 'line-chart',
+      resize: true,
+      data: [
+        {y: '2011 Q1', item1: 2666},
+        {y: '2011 Q2', item1: 2778},
+        {y: '2011 Q3', item1: 4912},
+        {y: '2011 Q4', item1: 3767},
+        {y: '2012 Q1', item1: 6810},
+        {y: '2012 Q2', item1: 5670},
+        {y: '2012 Q3', item1: 4820},
+        {y: '2012 Q4', item1: 15073},
+        {y: '2013 Q1', item1: 10687},
+        {y: '2013 Q2', item1: 8432}
+      ],
+      xkey: 'y',
+      ykeys: ['item1'],
+      labels: ['Item 1'],
+      lineColors: ['#3c8dbc'],
+      hideHover: 'auto'
+    });
+</script>
